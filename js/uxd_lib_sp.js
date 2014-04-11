0bf0fd69b0faf143a095ae0c5af064dcc48c0d77
道口彩 <douguchiaya@michikuchiirodori-no-MacBook-Pro.local>
2014-04-11 14:51:04 +0900

    uacheck

--- js/uxd_lib_sp.js
+++ js/uxd_lib_sp.js
@@ -3,8 +3,8 @@
 var UXDLIB_SP = {};
 
 //tap
-(function(UXDLIB_SP){
-  UXDLIB_SP.tap = function(args){
+(function (UXDLIB_SP) {
+  UXDLIB_SP.tap = function (args) {
     this.targetClassName = document.getElementsByClassName(args.targetClassName);
     this.addClassName = args.addClassName;
 
@@ -13,33 +13,33 @@ var UXDLIB_SP = {};
 
   var fn = UXDLIB_SP.tap.prototype;
 
-  fn.init = function(){
+  fn.init = function () {
     var self = this;
-    for(var i = 0 ,I = this.targetClassName.length; i < I; i++){
-      (function(l) {
-        self.targetClassName[l].addEventListener('touchstart', function(){
+    for (var i = 0 , I = this.targetClassName.length; i < I; i++) {
+      (function (l) {
+        self.targetClassName[l].addEventListener('touchstart', function () {
           self.addTapped(l);
-        },false);
+        }, false);
       })(i);
 
-      (function(l) {
-         self.targetClassName[l].addEventListener('touchmove', function(){
-           self.removeTapped(l);
-        },false);
-      })(i);
-
-      (function(l) {
-        self.targetClassName[l].addEventListener('touchend', function(){
+      (function (l) {
+        self.targetClassName[l].addEventListener('touchmove', function () {
           self.removeTapped(l);
-        },false);
+        }, false);
+      })(i);
+
+      (function (l) {
+        self.targetClassName[l].addEventListener('touchend', function () {
+          self.removeTapped(l);
+        }, false);
       })(i);
     }
 
-    fn.addTapped = function(num){
+    fn.addTapped = function (num) {
       this.targetClassName[num].classList.add(this.addClassName);
     };
 
-    fn.removeTapped = function(num){
+    fn.removeTapped = function (num) {
       this.targetClassName[num].classList.remove(this.addClassName);
     };
   }
@@ -48,6 +48,7 @@ var UXDLIB_SP = {};
       this.triggerClassName = document.getElementsByClassName(args.triggerClassName);
       this.targetClassName = args.targetClassName;
       this.bgModal = args.bgModal;
+      this.topPadding = args.topPadding;
       this.triggerClose = document.getElementsByClassName(args.triggerClose);
       this.padding = args.padding;
       this.bindSelect = args.bindSelect;
@@ -60,68 +61,65 @@ var UXDLIB_SP = {};
 
   var fn = UXDLIB_SP.modal.prototype;
 
-  fn.init = function(){
+  fn.init = function () {
     var self = this;
 
-    if(self.loadEvent){
-      document.addEventListener('DOMContentLoaded', function(evt){
+    if (self.loadEvent) {
+      document.addEventListener('DOMContentLoaded', function (evt) {
         self.addWindow(0, document, window, evt);
-      },false);
+      }, false);
     }
 
-    for(var i = 0 ,I = self.triggerClassName.length; i < I; i++){
-      (function(l) {
-          self.triggerClassName[l].addEventListener(self.bindSelect, function(evt){
-            self.addWindow(l, document, window, evt);
-          },false);
+    for (var i = 0 , I = self.triggerClassName.length; i < I; i++) {
+      (function (l) {
+        self.triggerClassName[l].addEventListener(self.bindSelect, function (evt) {
+          self.addWindow(l, document, window, evt);
+        }, false);
       })(i);
     }
 
-    for(var i = 0 ,I = self.triggerClose.length; i < I; i++){
-      (function(l) {
-      self.triggerClose[l].addEventListener(self.bindSelect, function(evt){
-        console.log(self);
+    for (var i = 0 , I = self.triggerClose.length; i < I; i++) {
+      (function (l) {
+        self.triggerClose[l].addEventListener(self.bindSelect, function (evt) {
+          console.log(self);
+          self.removeWindow(evt, document);
+        }, false);
+      })(i);
+    }
+
+    document.body.addEventListener(self.bindSelect, function (evt) {
+      if (evt.target.className === self.bgModal + ' showModal') {
         self.removeWindow(evt, document);
-      },false);
-      })(i);
+      }
+    }, false);
+
+  }
+  fn.addWindow = function (num, doc, win, e) {
+    var modalWrp = doc.getElementsByClassName(this.targetClassName),
+        bgModalWrp = doc.getElementsByClassName(this.bgModal);
+    bgModalWrp[0].classList.add('showModal');
+    bgModalWrp[0].setAttribute('style', 'width:' + doc.body.clientWidth + 'px;height:' + doc.body.clientHeight + 'px;');
+    if (this.modalShingleMode) {
+      modalWrp[0].classList.add('showModal');
+      modalWrp[0].setAttribute('style', 'top:' + e.view.scrollY + 'px;');
+    } else if (this.loadEvent) {
+      modalWrp[0].classList.add('showModal');
+    } else {
+      modalWrp[num].classList.add('showModal');
+      modalWrp[num].setAttribute('style', 'top:' + e.view.scrollY + 'px;');
     }
+    if (this.callBackOpen) {
+      this.callBackOpen(e);
+    }
+  };
 
-
-
-    document.body.addEventListener(self.bindSelect, function(evt){
-      if(evt.target.className === self.bgModal + ' showModal') {
-      self.removeWindow(evt, document);
-      }
-    },false);
-
-    fn.addWindow = function(num, doc, win, e){
-      var modalWrp = doc.getElementsByClassName(this.targetClassName),
-          bgModalWrp = doc.getElementsByClassName(this.bgModal);
-        bgModalWrp[0].classList.add('showModal');
-        bgModalWrp[0].setAttribute('style', 'width:' + doc.body.clientWidth + 'px;height:' + doc.body.clientHeight + 'px;');
-      if(this.modalShingleMode){
-        modalWrp[0].classList.add('showModal');
-        modalWrp[0].setAttribute('style','top:' + (e.view.scrollY + this.padding) + 'px;left:' + e.view.scrollX + 'px;');
-      } else if(this.loadEvent){
-        modalWrp[0].classList.add('showModal');
-        modalWrp[0].setAttribute('style','top:' + (0 + this.padding) + 'px;left:' + this.padding + 'px;');
-      }else{
-        modalWrp[num].classList.add('showModal');
-        modalWrp[num].setAttribute('style','top:' + (e.view.scrollY + this.padding) + 'px;left:' + e.view.scrollX + 'px;');
-      }
-      if(this.callBackOpen){
-        this.callBackOpen(e);
-      }
-    };
-
-    fn.removeWindow = function(e, doc){
-      doc.getElementsByClassName(this.bgModal)[0].classList.remove('showModal');
-      if(this.modalShingleMode){
-        doc.getElementsByClassName(this.targetClassName)[0].classList.remove('showModal');
-      }else {
-        for(var m = 0 ,M = doc.getElementsByClassName(this.targetClassName).length; m < M; m++){
-          doc.getElementsByClassName(this.targetClassName)[m].classList.remove('showModal');
-        }
+  fn.removeWindow = function (e, doc) {
+    doc.getElementsByClassName(this.bgModal)[0].classList.remove('showModal');
+    if (this.modalShingleMode) {
+      doc.getElementsByClassName(this.targetClassName)[0].classList.remove('showModal');
+    } else {
+      for (var m = 0 , M = doc.getElementsByClassName(this.targetClassName).length; m < M; m++) {
+        doc.getElementsByClassName(this.targetClassName)[m].classList.remove('showModal');
       }
     }
   }
